a9b597907a05ae51e4c1e4f29dd2884c34a76f22
Jo Soo Yoon <sooyoon126@gmail.com>
2021-08-21 15:22:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/soob1/rest/RestApiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.soob1.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiStudyApplication.class, args);
+	}
+
+}
