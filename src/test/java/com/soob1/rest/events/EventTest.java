9e8bc430b0ea3d6f46061f1e186106641ebdef46
Jo Soo Yoon <sooyoon126@gmail.com>
2021-08-22 20:01:19 +0900

    이벤트 비즈니스 로직 적용

--- src/test/java/com/soob1/rest/events/EventTest.java
+++ src/test/java/com/soob1/rest/events/EventTest.java
@@ -1,6 +1,8 @@
 package com.soob1.rest.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +28,53 @@ class EventTest {
 		// then
 		assertThat(event.getName()).isEqualTo(name);
 		assertThat(event.getDescription()).isEqualTo(description);
+	}
+
+	@ParameterizedTest
+	@MethodSource("paramsForTestFree")
+	public void testFree(int basePrice, int maxPrice, boolean isFree) {
+		// given
+		Event event = Event.builder()
+				.basePrice(basePrice)
+				.maxPrice(maxPrice)
+				.build();
+
+		// when
+		event.update();
+
+		// then
+		assertThat(event.isFree()).isEqualTo(isFree);
+	}
+
+	@ParameterizedTest
+	@MethodSource("paramsForTestOffline")
+	public void testOffline(String location, boolean isOffline) {
+		// given
+		Event event = Event.builder()
+				.location(location)
+				.build();
+
+		// when
+		event.update();
+
+		// then
+		assertThat(event.isOffline()).isEqualTo(isOffline);
+	}
+
+	private static Object[] paramsForTestFree() {
+		return new Object[] {
+				new Object[] {0, 0, true},
+				new Object[] {100, 0, false},
+				new Object[] {0, 100, false},
+				new Object[] {100, 200, false}
+		};
+	}
+
+	private static Object[] paramsForTestOffline() {
+		return new Object[] {
+				new Object[] {"강남역", true},
+				new Object[] {null, false},
+				new Object[] {"	", false}
+		};
 	}
 }
